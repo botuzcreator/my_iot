4ee79b43ddad60050712addb45afcf799d1ca8f8
botuzcreator <IISLOMNUR@GMAIL.COM>
2024-07-08 19:28:13 +0500

    Initial commit with express and static files

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const app = express();
 
 // MySQL bazasiga ulanish
 const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
