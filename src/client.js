4d8b09a821155601d7ec0266774a397085e4b00c
botuzcreator <IISLOMNUR@GMAIL.COM>
2024-07-09 13:26:02 +0500

    Loyihada o'zgarishlar qilindi

--- src/client.js
+++ src/client.js
@@ -1,40 +1,52 @@
 ﻿// WebSocket serverining URL manzilini kiriting
-const socketUrl = 'wss://myiot-production.up.railway.app:8080';
+var socketUrl = 'wss://myiot-production.up.railway.app:8080'; // Global o'zgaruvchi sifatida e'lon qilinmoqda
 let socket;
 
 function connectWebSocket() {
-    if (!socket || socket.readyState === WebSocket.CLOSED) {
-        socket = new WebSocket(socketUrl);
-    
-        socket.onopen = function(event) {
-            console.log('WebSocketga ulanildi.');
-        };
-    
-        socket.onerror = function(error) {
-            console.error('WebSocket xatosi:', error);
-        };
-    
-        socket.onclose = function(event) {
-            console.log('WebSocket ulanish yopildi. Qayta urinish boshlanmoqda...');
-            setTimeout(connectWebSocket, 5000); // 5 soniyadan keyin qayta urinish
-        };
-    
-        socket.onmessage = function(event) {
-            console.log('Qabul qilingan xabar: ' + event.data);
-            const data = event.data.split(';');
-            console.log('Split qilingan ma\'lumotlar:', data);
-            updateParameters(data);
-        };
-    }
-}
+    socket = new WebSocket(socketUrl);
 
-function updateParameters(data) {
-    document.getElementById('param1').textContent = data[0] + ' ' + '°C';
-    document.getElementById('param2').textContent = data[1] + ' ' + '%';
-    document.getElementById('param3').textContent = data[2] + ' ' + 'lx';
-    document.getElementById('param4').textContent = data[3] + ' ' + 'W/m²';
-    document.getElementById('param5').textContent = data[4];
-    document.getElementById('param6').textContent = data[5];
+    socket.onopen = function(event) {
+        console.log('WebSocketga ulanildi.');
+    };
+
+    socket.onerror = function(error) {
+        console.error('WebSocket xatosi:', error);
+    };
+
+    socket.onclose = function(event) {
+        console.log('WebSocket ulanish yopildi. Qayta urinish boshlanmoqda...');
+        setTimeout(connectWebSocket, 5000); // 5 soniyadan keyin qayta urinish
+    };
+
+    socket.onmessage = function(event) {
+        if (event.data instanceof Blob) {
+            const reader = new FileReader();
+            reader.onload = function() {
+                const message = reader.result;
+                console.log('Qabul qilingan xabar: ' + message);
+                const data = message.split(';');
+                console.log('Split qilingan ma\'lumotlar:', data);
+                document.getElementById('param1').textContent = data[0] + ' ' + '°C';
+                document.getElementById('param2').textContent = data[1] + ' ' + '%';
+                document.getElementById('param3').textContent = data[2] + ' ' + 'lx';
+                document.getElementById('param4').textContent = data[3] + ' ' + 'W/m²';
+                document.getElementById('param5').textContent = data[4];
+                document.getElementById('param6').textContent = data[5];
+            };
+            reader.readAsText(event.data);
+        } else {
+            const message = event.data.toString();
+            console.log('Qabul qilingan xabar: ' + message);
+            const data = message.split(';');
+            console.log('Split qilingan ma\'lumotlar:', data);
+            document.getElementById('param1').textContent = data[0] + ' ' + '°C';
+            document.getElementById('param2').textContent = data[1] + ' ' + '%';
+            document.getElementById('param3').textContent = data[2] + ' ' + 'lx';
+            document.getElementById('param4').textContent = data[3] + ' ' + 'W/m²';
+            document.getElementById('param5').textContent = data[4];
+            document.getElementById('param6').textContent = data[5];
+        }
+    };
 }
 
 connectWebSocket(); // WebSocketga ulanish funksiyasini chaqiramiz
