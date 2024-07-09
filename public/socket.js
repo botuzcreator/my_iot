7bbc96d53b024f1500e1d5f57c489ad5491a9905
botuzcreator <IISLOMNUR@GMAIL.COM>
2024-07-09 14:36:22 +0500

    Loyihada o'zgarishlar qilindi

---
+++ public/socket.js
@@ -0,0 +1,13 @@
+﻿const socket = new WebSocket('ws://myiot-production.up.railway.app:8080');
+
+socket.onopen = function() {
+  console.log('WebSocket connection established.');
+};
+
+socket.onerror = function(error) {
+  console.error('WebSocket Error:', error);
+};
+
+socket.onclose = function() {
+  console.log('WebSocket connection closed.');
+};
