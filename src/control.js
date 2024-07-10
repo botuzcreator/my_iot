6747e5c22f5c35387dae475d0ecde12a459a5417
botuzcreator <IISLOMNUR@GMAIL.COM>
2024-07-11 00:49:18 +0500

    Update project

--- src/control.js
+++ src/control.js
@@ -1,12 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const setButton = document.querySelector('#setButton');
-  const inputs = document.querySelectorAll('input');
+    const setButton = document.querySelector('button'); // SET tugmasini tanlash
+    const inputs = document.querySelectorAll('input'); // Barcha inputlarni tanlash
+  
+    setButton.addEventListener('click', () => {
+      const values = Array.from(inputs).map(input => input.value); // Input qiymatlarini o'qib olish
+      const message = { type: 'control-data', data: values.join(';'), deviceId: 'esp8266' }; // "control-data" va qiymatlarni birlashtirish
 
-  setButton.addEventListener('click', () => {
-    const values = Array.from(inputs).map(input => input.value);
-    const message = { type: 'control-data', data: values.join(';'), deviceId: 'esp8266' };
-    console.log('Brauzerdan qabul qilindi:', message);
-    socket.send(JSON.stringify(message));
-    console.log('Yuborilgan xabar:', JSON.stringify(message));
+      //konsolga chiqarish
+      console.log('Brauzerdan qabul qilindi:', message);
+      
+  
+      // WebSockets orqali serverga yuborish
+      const socket = new WebSocket('wss:myiot-production.up.railway.app:443'); // WebSocket server manzili
+      socket.onopen = () => {
+        // Serverga xabar yuborish
+        socket.send(JSON.stringify(message));
+        console.log('Yuborilgan xabar:', JSON.stringify(message));
+      };
+  
+      
+  
+      socket.onerror = (error) => {
+        console.error('WebSocket xatosi:', error);
+      };
+    });
   });
-});
+  
